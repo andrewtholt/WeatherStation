88a04cbe3eb1b618e3998b06b0992aa852d1908a
Andrew Holt <andrewtholt60@gmail.com>
2022-02-17 08:59:39 +0000

    wemoStart.sh:Reduced cycle time from 15 to 10 minutes.

--- microPython/wemoStart.sh
+++ microPython/wemoStart.sh
@@ -3,4 +3,4 @@
 
 mosquitto_pub -h 192.168.10.124  -t /home/office/WEMOS/RUN -m "YES" -r
 
-mosquitto_pub -h 192.168.10.124  -t /home/office/WEMOS/SLEEP_TIME -m "900" -r
+mosquitto_pub -h 192.168.10.124  -t /home/office/WEMOS/SLEEP_TIME -m "600" -r
